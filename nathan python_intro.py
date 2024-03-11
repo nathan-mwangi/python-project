f70ef7a8ce65ee606b9aceadd487ce19f7bcd50f
Githaiga05 <ggithaiga379@gmail.com>
2024-03-11 06:51:08 -0400

    additional python files

---
+++ nathan python_intro.py
@@ -0,0 +1,46 @@
+#pyhon comment -single line comment
+"""
+this is a sample multiline comment in python
+"""
+
+
+
+
+#variables
+student_name ="Jonathan"#data type is string
+student_age=20 #data type is integer
+student_fee=100.0 #data type is float
+student_marks=100
+is_male =True #data type is boolean
+
+
+
+
+#outputting the values in the variables
+print(student_name)
+print(student_age)
+print(is_male)
+
+
+
+student_name ="Jonathan"
+student_name =("Allan")
+print(student_name)
+
+
+
+
+STUDENT_NAME ="Ryan"
+student_name ="ben"
+print(STUDENT_NAME)
+
+#variable naming in python
+book_price=20.0 #valid variable name
+_book_price=20.0 #valid variable name
+#*book_price=10.0
+#123_book_price=0
+book_price_123=40
+
+
+x=y=z=10 #assigning one value to multilple variables
+x,y,z=30,40,50 #multiple values different variables

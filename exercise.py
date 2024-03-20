5cc20c620f2862f5891c4776f8bf7cf6d5c98b05
Githaiga05 <ggithaiga379@gmail.com>
2024-03-20 06:43:22 -0400

    additional files

--- exercise.py
+++ exercise.py
@@ -2,6 +2,42 @@
 numbers=[2,34,57,78,55,994,43,9]
 print("the largest number is",max(numbers))
 
+#getting repeated elements in a list
+list2=["cheese","tomato","potato","tomato","ram","ram"]
+repeated_veg=set()
+non_repeated_veg=set()
+for item in list2:
+    if list2.count  (item)>1:
+        repeated_veg.add(item)
+        print("my repeated items are",repeated_veg)
+    else:
+        non_repeated_veg.add(item)
+print("the non_repeated items are",non_repeated_veg)
+
+
+
+#dictionaries
+mydict={"brand":"Ford","model":"mustang","year":2016}
+print("my old dictionary is",mydict)
+delete_keys=["brand","model",]
+
+
+for key in delete_keys:
+    del mydict[key]
+print("my new dictionary is",mydict)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # how to add items in a tuple
